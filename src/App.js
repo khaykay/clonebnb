e6aafa1640499eb09d4c2347ed000eb4f0539b46
khaykay <berryscott97@gmail.com>
2022-10-22 19:04:15 +0100

    third update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Categoryelement from "./components/Categoryelement";
 function App() {
   return (
     <>
       <Navbar></Navbar>
+      <Categoryelement></Categoryelement>
       <Footer></Footer>
     </>
   );
--- src/components/Categoryelement.jsx
+++ src/components/Categoryelement.jsx
@@ -1,19 +1,22 @@
 import React from "react";
+import "../styles/Categoryelement.css";
 
 function Categoryelement() {
   const name = "ashawo";
   const price = "$43";
   return (
     <div className="categoryelement">
-      <div className="image"></div>
-      <div className="caption">
-        <div className="left">
-          <span className="location bold">lagos, nigeria</span>
-          <span className="host"> {`hosted by ${name}`}</span>
-          <span className="date">november</span>
-          <span className="price"> {`${price} night`}</span>
+      <div className="categoryelement-wrapper">
+        <div className="image"></div>
+        <div className="caption">
+          <div className="left">
+            <span className="location bold capitalize">lagos, nigeria</span>
+            <span className="host capitalize"> {`Hosted by ${name}`}</span>
+            <span className="date capitalize">november</span>
+            <span className="price"> {`${price} night`}</span>
+          </div>
+          <div className="right">hello</div>
         </div>
-        <div className="right"></div>
       </div>
     </div>
   );
