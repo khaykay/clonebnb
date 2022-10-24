ce88020172a08895d88d4ccd79c7e444baa34b51
khaykay samuel <46075314+khaykay@users.noreply.github.com>
2022-10-24 13:41:54 +0100

    Create Layout.jsx

---
+++ src/components/Layout.jsx
@@ -0,0 +1,42 @@
+import React , { useState } from "react";
+import Categorybar from "./Categorybar";
+import Topbar from "./Topbar";
+import Footer from "./Footer";
+import CategoryElement from "./CategoryElement";
+import { categories } from "../db/categories";
+import Card from "./Card";
+
+
+
+function Layout() {
+    const defaultCategory  = categories[0].lists
+    
+    const [selectedCategory, setSelectedCategory ] = useState(1)
+    const [savedCategory, setSavedCategory ] = useState(defaultCategory)
+
+    const saveCategoryId = (id) =>{
+      setSelectedCategory(id)
+      const categoryPicked = categories.filter((i)=> {
+            return i.id === id
+      })
+      const list = categoryPicked[0].lists
+      setSavedCategory(list)
+    }
+
+  return (
+    <div>
+      <Topbar></Topbar>
+      <Categorybar selectedCategory={selectedCategory} saveCategoryId={saveCategoryId} categories={categories} />
+      <Card>
+      {
+        savedCategory && 
+      <CategoryElement savedCategory={savedCategory}/>
+
+      }
+      </Card>
+      <Footer/>
+    </div>
+  );
+}
+
+export default Layout;
