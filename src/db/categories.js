0f5c1c9bfa2a57f7af36eaf88f5784dd72c18127
khaykay samuel <46075314+khaykay@users.noreply.github.com>
2022-10-24 13:36:12 +0100

    Update categories.js

--- src/db/categories.js
+++ src/db/categories.js
@@ -1,72 +1,547 @@
 export const categories = [
-    {   
-        id: 1,
-        name: 'Beach',
-        icon:'beach_access'
-    },
-    {   
-        id: 2,
-        name: 'Castles',
-        icon:'castle'
-    },
-    {   
-        id: 3,
-        name: 'National parks',
-        icon:'park'
-    },
-    {   
-        id: 4,
-        name: 'Arctic',
-        icon:'waves'
-    },
-    {   
-        id: 5,
-        name: 'Design',
-        icon:'brush'
-    },
-    {   
-        id: 6,
-        name: 'Surfing',
-        icon:'surfing'
-    },
-    {   
-        id: 7,
-        name: 'Cabin',
-        icon:'cabin'
-    },
-    {   
-        id: 8,
-        name: 'Mansions',
-        icon:'apartment'
-    },
-    {   
-        id: 9,
-        name: 'Bread & breakfast',
-        icon:'breakfast_dining'
-    },
-    {   
-        id: 11,
-        name: 'Amazing pools',
-        icon:'pool'
-    },
-    {   
-        id: 12,
-        name: 'Islands',
-        icon:'landslide'
-    },
-    {   
-        id: 13,
-        name: 'Amazing views',
-        icon:'view_timeline'
-    },
-    {   
-        id: 14,
-        name: 'Lakes',
-        icon:'water'
-    },
-    {   
-        id: 15,
-        name: 'House boat',
-        icon:'houseboat'
-    }
-]
+  {
+    id: 1,
+    name: "Beach",
+    icon: "beach_access",
+    lists: [
+      {
+        image: "https://a0.muscache.com/im/pictures/3a93ce52-9ad9-4534-9e16-1219ee47b815.jpg?im_w=1200",
+        location: "Abuja",
+        host: "josh",
+        date: "nov 1-6",
+        price: "34",
+        rating: 4.3
+      },
+      {
+        image: "https://a0.muscache.com/im/pictures/d02373cb-a53c-4cbd-9558-86926a913a43.jpg?im_w=720",
+        location: "lagos",
+        host: "caleb",
+        date: "nov 1-6",
+        price: "36",
+        rating: 4.3
+      },
+      {
+        image: "https://a0.muscache.com/im/pictures/52b2a2e7-b8ee-4df0-87e9-c4773208ca79.jpg?im_w=720",
+        location: "Abuja",
+        host: "dimma",
+        date: "nov 1-6",
+        price: "40",
+        rating: 4.3
+      },
+      {
+        image: "https://a0.muscache.com/im/pictures/bbe41c76-66b0-4075-bc48-22a03a714cfa.jpg?im_w=1200",
+        location: "Abuja",
+        host: "kelsie",
+        date: "nov 1-6",
+        price: "57",
+        rating: 4.3
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Castles",
+    icon: "castle",
+    lists: [
+      {
+        image: "https://a0.muscache.com/im/pictures/5c55cead-7f03-435c-8a47-3b2e99e8aa37.jpg?im_w=1200",
+        location: "Abuja",
+        host: "kie kie",
+        date: "nov 1-6",
+        price: "48",
+         rating: 4.3
+      },
+      {
+        image: "https://a0.muscache.com/im/pictures/a9873889-8535-4757-9648-87e183514a77.jpg?im_w=1200",
+        location: "Abuja",
+        host: "kim",
+        date: "nov 1-6",
+        price: "62",
+        rating: 4.3
+      },
+      {
+        image: "https://a0.muscache.com/im/pictures/baf57991-a877-4f73-9957-e20aa44f7d5e.jpg?im_w=1200",
+        location: "Abuja",
+        host: "mummy wa",
+        date: "nov 1-6",
+        price: "100",
+        rating: 4.3
+      },
+      {
+        image: "https://a0.muscache.com/im/pictures/5222713a-fb9c-4afe-9fdd-ae6d4c5eab42.jpg?im_w=720",
+        location: "uyo",
+        host: "daddy wa",
+        date: "nov 1-6",
+        price: "150",
+        rating: 4.3
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "National parks",
+    icon: "park",
+    lists: [
+      {
+        image: "https://a0.muscache.com/im/pictures/583280d2-b9b4-4f2f-b1e2-eec83552fb08.jpg?im_w=1200",
+        location: "delta",
+        host: "bimbo",
+        date: "nov 1-6",
+        price: "130",
+        rating: 4.3
+      },
+      {
+        image: "https://a0.muscache.com/im/pictures/miso/Hosting-17934876/original/7c190b9e-908f-4e87-8eb9-945a3a617191.jpeg?im_w=1200",
+        location: "awka",
+        host: "jerry",
+        date: "nov 1-6",
+        price: "57",
+        rating: 4.3
+      },
+      {
+        image: "https://a0.muscache.com/im/pictures/prohost-api/Hosting-51298103/original/b8a479ac-ea23-4599-a7f7-8e8706ec1289.jpeg?im_w=1200",
+        location: "enugu",
+        host: "bimbo",
+        date: "nov 1-6",
+        price: "60",
+        rating: 4.3
+      },
+      {
+        image: "https://a0.muscache.com/im/pictures/prohost-api/Hosting-51298103/original/312d5f91-f1d3-43b2-84e5-fc9b9f2565e5.jpeg?im_w=720",
+        location: "Abuja",
+        host: "bose",
+        date: "nov 1-6",
+        price: "13",
+        rating: 4.3
+      }
+    ]
+  },
+  {
+    id: 4,
+    name: "Arctic",
+    icon: "waves",
+    lists: [
+      {
+        image: "https://a0.muscache.com/im/pictures/4379daf7-1c38-41ba-9ca6-aaa10002bdde.jpg?im_w=720",
+        location: "Abuja",
+        host: "khaykay",
+        date: "nov 1-6",
+        price: "21",
+         rating: 4.3
+      },
+      {
+        image: "https://a0.muscache.com/im/pictures/fd7b98f1-ee95-4bf2-ac8e-e31a85e70031.jpg?im_w=720",
+        location: "lokoja",
+        host: "buju",
+        date: "nov 1-6",
+        price: "88",
+         rating: 4.3
+      },
+      {
+        image: "https://a0.muscache.com/im/pictures/b475b272-04cf-4b7f-84c8-3021ddb9a089.jpg?im_w=720",
+        location: "Abuja",
+        host: "bxnx",
+        date: "nov 1-6",
+        price: "71",
+         rating: 4.3
+      },
+      {
+        image: "https://a0.muscache.com/im/pictures/miso/Hosting-33397784/original/82f765e6-73eb-49e3-ae8e-e2642015c61f.jpeg?im_w=720",
+        location: "Abuja",
+        host: "ovie",
+        date: "nov 1-6",
+        price: "55",
+         rating: 4.3
+      }
+    ],
+  },
+  {
+    id: 5,
+    name: "Design",
+    icon: "brush",
+    lists: [
+      {
+        image: "https://a0.muscache.com/im/pictures/fc077779-d9bf-4759-a1dd-5d7b1e28c073.jpg?im_w=720",
+        location: "Abuja",
+        host: "peggy",
+        date: "nov 1-6",
+        price: "67",
+         rating: 4.3
+      },
+      {
+        image: "https://a0.muscache.com/im/pictures/fe1ded9b-596a-4a80-a571-7d36d006c5e5.jpg?im_w=720",
+        location: "yaba",
+        host: "bimpe",
+        date: "nov 1-6",
+        price: "90",
+         rating: 4.3
+      },
+      {
+        image: "https://a0.muscache.com/im/pictures/miso/Hosting-52727251/original/08580968-3d3c-4fc6-95e9-450bc2852234.jpeg?im_w=1200",
+        location: "lekki",
+        host: "brezzy",
+        date: "nov 1-6",
+        price: "34",
+         rating: 4.3
+      },
+      {
+        image: "https://a0.muscache.com/im/pictures/miso/Hosting-52727251/original/a7fb1504-a642-47fe-94e5-b1e625c0cda8.jpeg?im_w=1440",
+        location: "Ajah",
+        host: "chris",
+        date: "nov 1-6",
+        price: "76",
+         rating: 4.3
+      }
+    ],
+  },
+  {
+    id: 6,
+    name: "Surfing",
+    icon: "surfing",
+    lists: [
+      {
+        image: "https://a0.muscache.com/im/pictures/df68d58f-74ba-4a7c-8e23-fd4b09135ad6.jpg?im_w=720",
+        location: "sangotedo",
+        host: "ninny",
+        date: "nov 1-6",
+        price: "54",
+         rating: 4.3
+      },
+      {
+        image: "https://a0.muscache.com/im/pictures/prohost-api/Hosting-15245861/original/2c5bb7b8-a775-4edb-8e97-eeb5b72283e0.jpeg?im_w=720",
+        location: "guzampe",
+        host: "niniola",
+        date: "nov 1-6",
+        price: "87",
+         rating: 4.3
+      },
+      {
+        image: "https://a0.muscache.com/im/pictures/miso/Hosting-567167091478464764/original/57f28cba-c6d2-411d-96a2-057c2a955ce8.jpeg?im_w=720",
+        location: "gudu",
+        host: "fred",
+        date: "nov 1-6",
+        price: "67",
+         rating: 4.3
+      },
+      {
+        image: "https://a0.muscache.com/im/pictures/3477a259-56fa-4902-8539-70423389171c.jpg?im_w=720",
+        location: "apo",
+        host: "gifty",
+        date: "nov 1-6",
+        price: "45",
+        rating: 4.3
+      }
+    ],
+  },
+  {
+    id: 7,
+    name: "Cabins",
+    icon: "cabin",
+    lists: [
+      {
+        image: "https://a0.muscache.com/im/pictures/7c5f068d-11c4-44eb-8e48-8624a264a646.jpg?im_w=1200",
+        location: "yenagua",
+        host: "sixtus",
+        date: "nov 1-6",
+        price: "44",
+         rating: 4.3
+      },
+      {
+        image: "https://a0.muscache.com/im/pictures/miso/Hosting-50848339/original/b487c583-dc09-4995-9f30-f5bca96a63e1.jpeg?im_w=720",
+        location: "onitsha",
+        host: "khloe",
+        date: "nov 1-6",
+        price: "66",
+         rating: 4.3
+      },
+      {
+        image: "https://a0.muscache.com/im/pictures/1b379218-d790-481e-9fa2-bf530082c375.jpg?im_w=720",
+        location: "Abuja",
+        host: "ashaw0",
+        date: "nov 1-6",
+        price: "55",
+         rating: 4.3
+      },
+      {
+        image: "https://a0.muscache.com/im/pictures/a13906a5-01c1-4a94-9bcd-03fed022088e.jpg?im_w=720",
+        location: "asaba",
+        host: "amaka",
+        date: "nov 1-6",
+        price: "44",
+        rating: 4.3
+      }
+    ],
+  },
+  {
+    id: 8,
+    name: "Mansions",
+    icon: "apartment",
+    lists: [
+      {
+        image: "https://a0.muscache.com/im/pictures/4149a2e5-1154-45af-aadc-0e5f8512e41a.jpg?im_w=720",
+        location: "warri",
+        host: "frank",
+        date: "nov 1-6",
+        price: "55",
+        rating: 4.3
+      },
+      {
+        image: "https://a0.muscache.com/im/pictures/47ec6dbe-2bdb-4022-a33e-f8e72bf31c3b.jpg?im_w=720",
+        location: "benin",
+        host: "theo",
+        date: "nov 1-6",
+        price: "78",
+        rating: 4.3
+      },
+      {
+        image: "https://a0.muscache.com/im/pictures/6a9cae57-dfe1-4ea6-a570-84856e20d410.jpg?im_w=720",
+        location: "edo",
+        host: "benny",
+        date: "nov 1-6",
+        price: "89",
+        rating: 4.3
+      },
+      {
+        image: "https://a0.muscache.com/im/pictures/683c7115-e408-4a40-ac01-05684b1d082d.jpg?im_w=720",
+        location: "kebbi",
+        host: "buike",
+        date: "nov 1-6",
+        price: "17",
+        rating: 4.3
+      }
+    ]
+  },
+  {
+    id: 9,
+    name: "Bed & breakfast",
+    icon: "breakfast_dining",
+    lists: [
+      {
+        image: "https://a0.muscache.com/im/pictures/6ca1a9bd-1bf5-4d14-ad4d-a6ecada36648.jpg?im_w=720",
+        location: "Abuja",
+        host: "cj",
+        date: "nov 1-6",
+        price: "99",
+        rating: 4.3
+      },
+      {
+        image: "https://a0.muscache.com/im/pictures/e49c73c0-1a87-4faf-bf9c-557b06b2ce17.jpg?im_w=720",
+        location: "Abuja",
+        host: "lorem",
+        date: "nov 1-6",
+        price: "40",
+        rating: 4.3
+      },
+      {
+        image: "https://a0.muscache.com/im/pictures/09dfa9c4-a33f-495b-977f-b9bfb5e589d7.jpg?im_w=1200",
+        location: "Abuja",
+        host: "imposum",
+        date: "nov 1-6",
+        price: "23",
+        rating: 4.3
+      },
+      {
+        image: "https://a0.muscache.com/im/pictures/prohost-api/Hosting-22154968/original/15ff4cc1-59c7-4338-8f26-d869fb458955.jpeg?im_w=720",
+        location: "bini-kebbi",
+        host: "maroon 5",
+        date: "nov 1-6",
+        price: "66",
+        rating: 4.3
+      }
+    ]
+  },
+  {
+    id: 10,
+    name: "Amazing pools",
+    icon: "pool",
+    lists: [
+      {
+        image: "https://a0.muscache.com/im/pictures/miso/Hosting-665927085243769423/original/647deb44-3c28-4219-8701-5f07c9748bff.png?im_w=720",
+        location: "kogi",
+        host: "adam",
+        date: "nov 1-6",
+        price: "56",
+        rating: 4.3
+      },
+      {
+        image: "https://a0.muscache.com/im/pictures/f1edd7e5-bdc1-4e3a-aeea-6c42904ae542.jpg?im_w=1200",
+        location: "Abuja",
+        host: "ben",
+        date: "nov 1-6",
+        price: "78",
+        rating: 4.3
+      },
+      {
+        image: "https://a0.muscache.com/im/pictures/5cd1f028-a09e-4c82-b5d5-f9f977edcdeb.jpg?im_w=720",
+        location: "aba",
+        host: "jonah",
+        date: "nov 1-6",
+        price: "21",
+        rating: 4.3
+      },
+      {
+        image: "https://a0.muscache.com/im/pictures/e9037b5a-25d6-4ec4-a1d0-9545f4220700.jpg?im_w=720",
+        location: "Abuja",
+        host: "batta.ng",
+        date: "nov 1-6",
+        price: "67",
+        rating: 4.3
+      }],
+  },
+  {
+    id: 11,
+    name: "Islands",
+    icon: "landslide",
+    lists: [
+      {
+        image: "https://a0.muscache.com/im/pictures/miso/Hosting-555632030522710376/original/c8b5a0c1-0468-4402-a772-2b93d75366df.jpeg?im_w=720",
+        location: "Abuja",
+        host: "sixxine",
+        date: "nov 1-6",
+        price: "79",
+        rating: 4.3
+      },
+      {
+        image: "https://a0.muscache.com/im/pictures/prohost-api/Hosting-48729525/original/f62e1b1a-d6af-438b-82ce-95c51a4e0ca1.jpeg?im_w=720",
+        location: "Akure",
+        host: "400",
+        date: "nov 1-6",
+        price: "90",
+        rating: 4.3
+      },
+      {
+        image: "https://a0.muscache.com/im/pictures/prohost-api/Hosting-50615181/original/001dbb4c-a83c-458f-a45c-cf89af86d81a.jpeg?im_w=720",
+        location: "oyo",
+        host: "busty",
+        date: "nov 1-6",
+        price: "78",
+        rating: 4.3
+      },
+      {
+        image: "https://a0.muscache.com/im/pictures/24955679/df9452e8_original.jpg?im_w=1200",
+        location: "Abuja",
+        host: "shatta bandle",
+        date: "nov 1-6",
+        price: "88",
+        rating: 4.3
+      }
+    ]
+  },
+  {
+    id: 12,
+    name: "Amazing views",
+    icon: "view_timeline",
+    lists: [
+      {
+        image: "https://a0.muscache.com/im/pictures/7feb9bfe-4799-41dd-b01f-67f6c508a299.jpg?im_w=720",
+        location: "Abuja",
+        host: "shatta wale",
+        date: "nov 1-6",
+        price: "67",
+        rating: 4.3
+      },
+      {
+        image: "https://a0.muscache.com/im/pictures/68432800/c3f8875f_original.jpg?im_w=720",
+        location: "kano",
+        host: "chicken",
+        date: "nov 1-6",
+        price: "77",
+        rating: 4.3
+      },
+      {
+        image: "https://a0.muscache.com/im/pictures/40c561d0-5ede-4411-80eb-49aca488a409.jpg?im_w=720",
+        location: "kaduna",
+        host: "cowwie",
+        date: "nov 1-6",
+        price: "55",
+        rating: 4.3
+      },
+      {
+        image: "https://a0.muscache.com/im/pictures/2e15ea81-9fe7-4692-83e5-65a90a84f3d6.jpg?im_w=1200",
+        location: "Abuja",
+        host: "goatie",
+        date: "nov 1-6",
+        price: "73",
+        rating: 4.3
+      }
+    ]
+  },
+  {
+    id: 13,
+    name: "Lakes",
+    icon: "water",
+    lists: [
+      {
+        image: "https://a0.muscache.com/im/pictures/3396b92b-5f4d-43ea-a0f2-022e1635860e.jpg?im_w=720",
+        location: "kastina",
+        host: "sam",
+        date: "nov 1-6",
+        price: "76",
+        rating: 4.3
+      },
+      {
+        image: "https://a0.muscache.com/im/pictures/prohost-api/Hosting-50887516/original/ebfdb960-c199-4b08-ab38-4c9bfde75cca.jpeg?im_w=720",
+        location: "Abuja",
+        host: "boo boo",
+        date: "nov 1-6",
+        price: "77",
+        rating: 4.3
+      },
+      {
+        image: "https://a0.muscache.com/im/pictures/prohost-api/Hosting-51162585/original/54e014c3-da18-4ea8-96ea-05bd5b98fe32.jpeg?im_w=720",
+        location: "uyo",
+        host: "xavier",
+        date: "nov 1-6",
+        price: "87",
+        rating: 4.3
+      },
+      {
+        image: "https://a0.muscache.com/im/pictures/miso/Hosting-47503742/original/0664809c-2157-44eb-9348-67126841d4f1.jpeg?im_w=720",
+        location: "orlu",
+        host: "xavy",
+        date: "nov 1-6",
+        price: "56",
+        rating: 4.3
+      }
+    ]
+  },
+  {
+    id: 14,
+    name: "House boat",
+    icon: "houseboat",
+    lists: [
+      {
+        image: "https://a0.muscache.com/im/pictures/prohost-api/Hosting-53028879/original/15e423aa-4ebe-46ae-a0e9-7c1d120ca13e.jpeg?im_w=720",
+        location: "Abuja",
+        host: "justin",
+        date: "nov 1-6",
+        price: "99",
+        rating: 4.3
+      },
+      {
+        image: "https://a0.muscache.com/im/pictures/41487bfe-c155-4c71-9834-3e4e3e26e055.jpg?im_w=720",
+        location: "imo",
+        host: "chuks",
+        date: "nov 1-6",
+        price: "10",
+        rating: 4.3
+      },
+      {
+        image: "https://a0.muscache.com/im/pictures/32d4fd7e-e678-4e6a-a7c2-21e30629b762.jpg?im_w=720",
+        location: "kwara",
+        host: "becky",
+        date: "nov 1-6",
+        price: "67",
+        rating: 4.3
+      },
+      {
+        image: "https://a0.muscache.com/im/pictures/miso/Hosting-52376294/original/9ffe28d6-e9b5-4991-9582-d6621aaa9c98.jpeg?im_w=720",
+        location: "ilorin",
+        host: "kosi",
+        date: "nov 1-6",
+        price: "77",
+        rating: 4.3
+      }
+    ]
+  },
+];
