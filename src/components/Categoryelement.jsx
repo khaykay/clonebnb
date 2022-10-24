6a4b44032b9da882d283f411d448d11b902554b6
khaykay samuel <46075314+khaykay@users.noreply.github.com>
2022-10-24 13:37:28 +0100

    Update Categoryelement.jsx

--- src/components/Categoryelement.jsx
+++ src/components/Categoryelement.jsx
@@ -1,31 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/Categoryelement.css";
 
-function Categoryelement() {
-  const name = "ashawo";
-  const price = "$43";
+function CategoryElement({ savedCategory }) {
+  
+const [favourites, setFavourites] = useState([])
+useEffect(()=>{
+  setFavourites([])
+},[savedCategory])
+
+const saveCategoryId = (id) => {
+  let array = [...favourites]
+let updatedArray = []
+  if(array.includes(id)){
+    updatedArray = array.filter(i => i !== id)
+  }
+  else {
+    updatedArray = [...favourites, id]
+  }
+
+  setFavourites(updatedArray)
+  
+}
+
   return (
-    <div className="categoryelement">
-      <div className="categoryelement-wrapper">
-        <div className="image"></div>
-        <div className="caption">
-          <div className="left">
-            <span className="location bold capitalize">lagos, nigeria</span>
-            <span className="host capitalize"> {`Hosted by ${name}`}</span>
-            <span className="date capitalize">november</span>
-            <span className="price">
-              <span className="bold">{price}</span>
-              {` night`}
-            </span>
+    <div className="category-element">
+      {
+        savedCategory?.length && savedCategory?.map((item, index) => (
+          <div className="categoryelement-wrapper" key={index}>
+            <div className="image" >
+            {/* style={{ backgroundImage: `url(${item.image})` }} */}
+            <i 
+            className={`${favourites.includes(index) ? 'favourite-active' : 'fa-icon'} material-icons`} onClick={()=>saveCategoryId(index)}
+           >favorite</i>
+           <img src={item.image} alt="" />
+            </div>
+            <div className="caption">
+              <div className="left">
+                <span className="location bold capitalize">{`${item.location} , Nigeria`}</span>
+                <span className="host capitalize"> {`Hosted by ${item.host}`}</span>
+                <span className="date capitalize">{item.date}</span>
+                <span className="price">
+                  <span className="bold">{`$${item.price}`}</span>
+                  {` night`}
+                </span>
+              </div>
+              <div className="right">
+                <span className="star"></span>
+                <span className="rating">{item.rating}</span>
+              </div>
+            </div>
           </div>
-          <div className="right">
-            <span className="star"></span>
-            <span className="rating"></span>
-          </div>
-        </div>
-      </div>
+
+        ))
+      }
     </div>
   );
 }
 
-export default Categoryelement;
+export default CategoryElement;
